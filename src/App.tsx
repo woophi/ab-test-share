f6b057a9748e869817160b07cdcf9f9ea6738044
Konstantin Mikheev <mik.kon6610@gmail.com>
2024-01-18 13:14:44 +0100

    call via GamesWebViewAdapter

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 
 declare global {
   interface Window {
-    shareData: (
-      title: string,
-      description: string,
-      imgUrl: string,
-      shareUrl: string
-    ) => void;
+    GamesWebViewAdapter: {
+      shareData: (
+        title: string,
+        description: string,
+        imgUrl: string,
+        shareUrl: string
+      ) => void;
+    };
   }
 }
 
@@ -18,7 +20,7 @@ function App() {
       <div className="card">
         <button
           onClick={() =>
-            window.shareData(
+            window.GamesWebViewAdapter.shareData(
               "test",
               "test desc",
               "https://sun9-42.userapi.com/impg/G65QFMX3rtf71ffHkazr8MwGO3RtXPdvP83CXg/cxXCY-dMSe0.jpg?size=762x509&quality=96&sign=9650f92a968de904a177b9f75acfde25&type=album",
